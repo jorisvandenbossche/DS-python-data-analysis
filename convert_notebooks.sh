37e4b87a0d88940f22453dd645c1338d45afd327
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-12-21 00:32:28 +0100

    Final proofread

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -21,12 +21,14 @@ declare -a arr=(
                 #"pandas_02_basic_operations.ipynb" 
                 #"pandas_03_selecting_data.ipynb"
                 #"pandas_04_time_series_data.ipynb"
-                "pandas_05_combining_datasets.ipynb"
-                "pandas_06_groupby_operations.ipynb"
-                "pandas_07_reshaping_data.ipynb"
+                #"pandas_05_combining_datasets.ipynb"
+                #"pandas_06_groupby_operations.ipynb"
+                #"pandas_07_reshaping_data.ipynb"
                 #"case1_bike_count.ipynb"
-                "case2_biodiversity_cleaning.ipynb"
-                "case2_biodiversity_analysis.ipynb"
+                #"case2_biodiversity_cleaning.ipynb"
+                #"case2_biodiversity_analysis.ipynb"
+                "case3_air_quality_processing.ipynb"
+                "case3_air_quality_analysis.ipynb"
                 )
 
 
