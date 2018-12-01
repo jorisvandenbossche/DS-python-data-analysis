c079653b48cabb503d46fd475ea6550eaa033944
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-12-01 14:09:54 +0100

    Update script to automatically convert notebooks/generate solutions

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,7 +1,5 @@
-# run this from the _solved directory
-# it creates there a notebooks/ and _solutions/ dir
-# then manually copy this to the ../notebooks dir
-
+# run this from the the top-level directory
+# will copy the generated notebooks and solutions into the /notebooks dir
 
 declare -a arr=(
                 #"00-jupyter_introduction.ipynb"
@@ -11,32 +9,41 @@ declare -a arr=(
                 #"04-reusing_code.ipynb"
                 #"05-numpy.ipynb"
                 #"python_rehearsal"
-                #"pandas_01_data_structures.ipynb"
-                #"pandas_02_basic_operations.ipynb" 
-                #"pandas_03_selecting_data.ipynb"
-                #"pandas_04_time_series_data.ipynb"
-                #"pandas_05_combining_datasets.ipynb"
-                #"pandas_06_groupby_operations.ipynb"
-                #"pandas_07_reshaping_data.ipynb"
-                #"visualization_01_matplotlib.ipynb"
-                #"visualization_02_plotnine.ipynb"
+                "pandas_01_data_structures.ipynb"
+                "pandas_02_basic_operations.ipynb"
+                "pandas_03_selecting_data.ipynb"
+                "pandas_04_time_series_data.ipynb"
+                "pandas_05_combining_datasets.ipynb"
+                "pandas_06_groupby_operations.ipynb"
+                "pandas_07_reshaping_data.ipynb"
+                "visualization_01_matplotlib.ipynb"
+                "visualization_02_plotnine.ipynb"
                 "visualization_03_landscape.ipynb"
-                #"case1_bike_count.ipynb"
-                #"case2_biodiversity_processing.ipynb"
-                #"case2_biodiversity_analysis.ipynb"
-                #"case3_bacterial_resistance_lab_experiment"
-
-                #"case4_air_quality_processing.ipynb"
-                #"case4_air_quality_analysis.ipynb"
+                "case1_bike_count.ipynb"
+                "case2_biodiversity_processing.ipynb"
+                "case2_biodiversity_analysis.ipynb"
+                "case3_bacterial_resistance_lab_experiment"
                 "case4_air_quality_processing.ipynb"
                 "case4_air_quality_analysis.ipynb"
                 )
 
+cd _solved
+
+mkdir ./notebooks
+
+echo "- Converting notebooks"
 
 for i in "${arr[@]}"
 do
-   echo "$i"
-   # or do whatever with individual element of the array
-   
+   echo "--" "$i"
    jupyter nbconvert --to=notebook --config ../nbconvert_config.py --output "notebooks/$i" "$i"   
 done
+
+echo "- Copying converted notebooks and solutions"
+cp -r notebooks/. ../notebooks
+cp -r _solutions/. ../notebooks/_solutions
+
+rm -r notebooks/
+rm -r _solutions/
+
+cd ..
--- nbconvert_config.py
+++ nbconvert_config.py
@@ -1,3 +1,1 @@
-c.Exporter.preprocessors = ['nbconvert.preprocessors.ClearExercisePreprocessor', 'nbconvert.preprocessors.ClearOutputPreprocessor']
-
-#c.Exporter.preprocessors = ['nbtutor.ClearExercisePreprocessor', 'nbconvert.preprocessors.ClearOutputPreprocessor']
+c.Exporter.preprocessors = ['nbtutor.ClearExercisePreprocessor', 'nbconvert.preprocessors.ClearOutputPreprocessor']
