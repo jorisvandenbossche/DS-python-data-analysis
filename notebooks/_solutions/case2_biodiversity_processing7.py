8f0cbdfa88ac3da3780826fb69d06c5d3e28ca58
Stijn Van Hoey <stijnvanhoey@gmail.com>
2019-06-02 20:35:58 +0200

    Update june 2019 (#88)
    Co-authored-by: stijnvanhoey <stijnvanhoey@gmail.com>
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- notebooks/_solutions/case2_biodiversity_processing7.py
+++ notebooks/_solutions/case2_biodiversity_processing7.py
@@ -1,4 +1,4 @@
 survey_data_decoupled = solve_double_field_entry(survey_data.copy(), 
                                                  "and", 
-                                                 column="species") # get help of the function by CTRL + TAB
+                                                 column="species") # get help of the function by SHIFT + TAB
 # REMARK: the copy() statement here (!) see pandas_03b_indexing.ipynb notebook `chained indexing section`
